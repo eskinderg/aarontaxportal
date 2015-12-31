3b967306319cbcc99c37e1ba73e015003d5de31b
eskinderg <eskinderget@gmail.com>
2015-12-31 05:25:59 -0500

    Documents Database Migration Created

--- app/Http/Controllers/documentContoller.php
+++ app/Http/Controllers/documentContoller.php
@@ -9,6 +9,8 @@ use Validator;
 use App\Http\Requests;
 use Request;
 use App\Http\Controllers\Controller;
+use Auth;
+use App\Document;
 
 class documentContoller extends Controller
 {
@@ -20,72 +22,6 @@ class documentContoller extends Controller
     public function index()
     {
       return view("document.index");
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     public function upload(Request $request)
@@ -105,6 +41,13 @@ class documentContoller extends Controller
                       $destinationPath = 'uploads';
                       $filename = $file->getClientOriginalName();
                       $upload_success = $file->move($destinationPath, $filename);
+
+                      $doc = new Document();
+                      $doc->filename = $filename;
+                      //$doc->user()=Auth::User();
+                      Auth::User()->documents()->save($doc);
+                      //$doc->save();
+
                       $uploadcount ++;
                   }
           }
